c95d97c8bc1b40e318846d51aab22f2e8332f0a3
peterd <peter@dematte.at>
2018-07-20 20:33:24 +0200

    Handlebars compatibility

--- handlebars.js
+++ handlebars.js
@@ -9,26 +9,21 @@ var Handlebars = function() {
     tools: function(_this, findData, fn, name, params, data, parts, body, altBody) {
       var key = {};
       var hash = {};
+      var value;
 
       for (var n = params.length; n--;) {
-        key = parts.rawParts[params[n]] || {
-          value: params[n],
-          keys: [params[n]],
-          depth: 0,
-        };
-        hash[params[n]] = params[n] =
-          key.isString ? key.value : findData(data, key.value, key.keys, key.depth);
+        key = parts.rawParts[params[n]] || { value: params[n], keys: [params[n]], depth: 0 };
+        hash[params[n]] = params[n] = key.isString ?
+          key.value : findData(data, key.value, key.keys, key.depth);
       }
 
-      var value = fn.apply(data.path[0], params.concat({
-          name: name,
-          hash: hash,
-          fn: function(_data) { return body && (data.path[0] = _data) && body(data) },
-          inverse: function(_data) {
-            return altBody && (data.path[0] = _data) && altBody(data);
-          },
-          data: { root: data.path[0] },
-        }));
+      value = fn.apply(data.path[0], params.concat({
+        name: name,
+        hash: hash,
+        fn: function(_data) { return body && (data.path[0] = _data) && body(data) },
+        inverse: function(_data) { return altBody && (data.path[0] = _data) && altBody(data) },
+        data: { root: data.path[0] },
+      }));
 
       return typeof value === 'function' ? value() : value; // SafeString()
     }
@@ -57,22 +52,24 @@ Handlebars.prototype = {
 function create() {
   var hb = new Handlebars();
 
-  hb.Utils = {
-    escapeExpression: function(string) {
-      return String(string).replace(hb.schnauzer.entityRegExp, function(char) {
-        return hb.schnauzer.options.entityMap[char];
-      });
-    },
-  };
-  hb.SafeString = function(string) { return function() { return string } };
   hb.createFrame = function(obj) {
     var frame = Object.extend({}, obj);
 
     frame._parent = obj;
     return frame;
   };
+  hb.escapeExpression = function(string) {
+    return String(string).replace(hb.schnauzer.entityRegExp, function(char) {
+      return hb.schnauzer.options.entityMap[char];
+    });
+  };
+  hb.SafeString = function(string) { return function() { return string } };
   hb.create = create;
   hb.default = hb;
+  hb.Utils = {
+    createFrame: hb.createFrame,
+    escapeExpression: hb.escapeExpression,
+  };
 
   return hb;
 }
