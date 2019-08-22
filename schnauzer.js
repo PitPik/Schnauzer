c21f8d6e0f454ae7e4115c852db6a87cf57afa48
Peter Dematté <PitPik@users.noreply.github.com>
2019-08-22 10:32:20 +0200

    add better attribute support; minor changes

--- schnauzer.js
+++ schnauzer.js
@@ -139,8 +139,8 @@ function crawlObjectUp(data, keys) { // faster than while
 }
 
 function check(data, altData, keys) {
-  return data !== undefined ? data : keys ?
-    crawlObjectUp(altData, keys) : altData;
+  return data !== undefined ? data :
+    keys ? crawlObjectUp(altData, keys) : altData;
 }
 
 function findData(data, key, keys, pathDepth) {
@@ -191,10 +191,6 @@ function getVar(text) {
       value = (path[0] === '@' && '@' || '') + path.pop();
       depth = path.length;
     }
-    name = name.replace(/^(?:\.|this)\//, function() {
-      strict = true;
-      return '';
-    });
     keys = value.split(/[\.\/]/);
 
     value = value.replace(/^(\.\/|this\.|this\/|\*)/, function(all, $1) {
