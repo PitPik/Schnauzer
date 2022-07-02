8b7960dc1196fb9c27dc8d5bdf5ee5858ab1ab41
Peter Dematté <PitPik@users.noreply.github.com>
2022-07-02 20:39:25 +0200

    optimise render speed

--- schnauzer.js
+++ schnauzer.js
@@ -127,7 +127,7 @@ function addScope(model, data, alias) {
   var scopes = model.scopes;
   var newLevel = model.alias ? [model.alias] : [];
   var level = concatArrays(scopes[0].level, newLevel);
-  var values = model.values || {};
+  var values = model.values;
   var prevAlias = scopes[1] ? cloneObject({}, scopes[1].alias) : {};
 
   model.alias = null; model.values = null; // TODO
@@ -152,11 +152,11 @@ function getDeepData(data, main, alias) {
   return { value: data[main.value], parent: data, variable: main, alias: alias || false };
 }
 
-function getAlias(level, main, scope, data) {
+function getAlias(level, main, scope, data, trackData) {
   for (var n = 0, l = level.length, helpers = scope.helpers; n < l; n++) {
     data = getDeepData(level[n], main, true);
     if (data.value !== undefined) {
-      if (scope = scope.alias[data.variable.value]) {
+      if (trackData && (scope = scope.alias[data.variable.value])) {
         data.parent = scope.parent;
         data.key = scope.key;
         if (helpers['@length']) data.helpers = helpers;
@@ -198,8 +198,10 @@ function getData(_this, model, tagData, out) {
     data = { value: scope.helpers[main.value], variable: main, parent: scope.helpers['@parent'],
       key: scope.helpers['@key'], helpers: scope.helpers };
 
-    if (data.value === undefined && scope.values) data = getAlias([scope.values], main, scope, data);
-    if (data.value === undefined && !main.isStrict) data = getAlias(scope.level, main, scope, data);
+    if (data.value === undefined && scope.values)
+      data = getAlias([scope.values], main, scope, data, trackData);
+    if (data.value === undefined && !main.isStrict)
+      data = getAlias(scope.level, main, scope, data, trackData);
     if (data.value === undefined) data = !main.helper ? getDeepData(scope.scope, main) :
       { value: renderHelper(_this, args = getData(_this, model, main, []), model, main) };
     if (data.value === undefined && model.extra) data = getDeepData(model.extra, main);
